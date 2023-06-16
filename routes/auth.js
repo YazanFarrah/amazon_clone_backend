44b45268ef5d1ad482d5970e6d16305861e4af92
YazanFarrah <yazanfarrah@gmail.com>
2023-06-16 16:02:23 +0300

    modified authjs middleware

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,9 @@ const express = require("express");
 const User = require("../models/user");
 const bcryptjs = require("bcryptjs");
 const authRouter = express.Router();
-
+const jwt = require("jsonwebtoken");
 //sign up route
+const auth = require("../middlewares/auth");
 authRouter.post("/api/signup", async (req, res) => {
   //get the data from the client
   try {
@@ -33,12 +34,47 @@ authRouter.post("/api/signup", async (req, res) => {
 
 //sign in route
 
-authRouter.post("/api/signin", (async) => {
+authRouter.post("/api/signin", async (req, res) => {
   try {
     const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ msg: "User with this email doesn't exist" });
+    }
+    const isMatch = await bcryptjs.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Incorrect password" });
+    }
+    const token = jwt.sign({ id: user._id }, "passwordKey");
+    res.json({ token, ...user._doc });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
+});
+
+authRouter.post("/api/tokenIsvalid", async (req, res) => {
+  try {
+    const token = req.header("x-auth-token");
+    if (!token) return res.json(false);
+    const verified = jwt.verify(token, "passwordKey");
+    if (!verified) return res.json(false);
+
+    const user = await User.findById(verified.id);
+    if (!user) return res.json(false);
+    res.json(true);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+//get user data
+
+authRouter.get("/", auth, async (req, res) => {
+  const user = await User.findById(req.user);
+  res.json({ ...user._doc, token: req.token });
 });
 
 module.exports = authRouter;

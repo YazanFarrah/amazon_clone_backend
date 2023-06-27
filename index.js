7f6e05d2d88e7be2c797232c6acceda965511d4d
YazanFarrah <yazanfarrah@gmail.com>
2023-06-27 03:44:10 +0300

    commented out socket io for deploying issues

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const authRouter = require("./routes/auth");
 const adminRoute = require("./routes/admin");
 const productRouter = require("./routes/product");
 const userRouter = require("./routes/user");
-const supportRouter = require("./routes/support");
+// const supportRouter = require("./routes/support");
 
 //init
 const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+// const io = socketio(server);
 
 const DB =
   "mongodb+srv://yazanfarrah03:OcqR6C85ei0OHPUS@cluster0.7nkhaij.mongodb.net/?retryWrites=true&w=majority";
@@ -26,7 +26,7 @@ app.use(authRouter);
 app.use(adminRoute);
 app.use(productRouter);
 app.use(userRouter);
-app.use(supportRouter);
+// app.use(supportRouter);
 
 //connections
 mongoose
@@ -35,22 +35,22 @@ mongoose
     console.log("MongoDB Connection successful");
 
     // Handle websocket connections
-    io.on("connection", (socket) => {
-      console.log("A user connected");
+    // io.on("connection", (socket) => {
+    //   console.log("A user connected");
 
-      // Handle chat messages
-      socket.on("chat message", (msg) => {
-        console.log("Received message:", msg);
+    //   // Handle chat messages
+    //   socket.on("chat message", (msg) => {
+    //     console.log("Received message:", msg);
 
-        // Broadcast the message to all connected clients
-        io.emit("chat message", msg);
-      });
+    //     // Broadcast the message to all connected clients
+    //     io.emit("chat message", msg);
+    //   });
 
-      // Handle disconnections
-      socket.on("disconnect", () => {
-        console.log("A user disconnected");
-      });
-    });
+    //   // Handle disconnections
+    //   socket.on("disconnect", () => {
+    //     console.log("A user disconnected");
+    //   });
+    // });
 
     server.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
